3ceedde24a6e8161bc2c7d5b883a680ff4eb2744
Chris Ryan <cryan@redhat.com>
2014-10-18 17:12:31 +0200

    Updated docs, added a timer for pinging users.

--- cucubot.rb
+++ cucubot.rb
@@ -2,11 +2,31 @@ require 'cinch'
 require 'json'
 require 'open-uri'
 
+#Borrowed heavily from https://raw.githubusercontent.com/cinchrb/cinch/master/examples/plugins/timer.rb
 class LaggardPlugin
   include Cinch::Plugin
-  timer 5, method: :check_scrums
+  #Every five hours (18000 seconds) run the 'check_scrums' method
+  timer 18000, method: :check_scrums
   def check_scrums
-    User('testuser').send("Hello!")
+    #Check the scrum5000 app for users who have and have not filled out a scrum
+    #If response is empty, everyone filled out a scrum, otherwise, send a private
+    #message to the person.
+    response = open("#{ENV['CUCUBOT_SCRUM5000']}/bot_portal/bot_checkpoint.json").read
+    if response.empty?
+      #Uncomment the following line to spam the channel with an all-clear message
+      #Channel(ENV['CUCUSHIFT_IRC_CHANNEL']).send "No laggards today!"
+      puts "No laggards today!"
+    else
+      parsed = JSON.parse(response)
+      parsed.each do |unreported|
+        #Uncomment the below line to spam the channel with users
+        #who have not yet filled out a scrum
+        #Channel(ENV['CUCUSHIFT_IRC_CHANNEL']).send "#{unreported[1]}"
+
+        #Add support for regex when user changes name
+        User("#{unreported[1]}").send("#{unreported[1]}, please fill out your scrum today.")
+      end
+    end
   end
 end
 
